dc33eb8553ea9616704e1b38f320e380ce00a881
Densyy <densyy@hotmail.com>
2023-09-24 07:24:01 -0300

    adiciona index

---
+++ index.js
@@ -0,0 +1,13 @@
+/* ---- Methods ---- */
+
+function densyyCors (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PUT,DELETE,PATCH')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, x-access-token')
+
+  if (req.method === 'OPTIONS') return res.sendStatus(204)
+
+  next()
+}
+
+module.exports = { densyyCors }
